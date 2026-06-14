fd4d0398a20d8d537e02dfe38a45b0f417bb25e0
antara <antarakashyapsta9@gmail.com>
2026-06-15 01:47:51 +0530

    change strict none

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ const generateToken=(res,payload)=>{
     res.cookie("token",token,{
         httpOnly:true,
         secure:process.env.NODE_ENV === "production",
-        sameSite:"lax",
+        sameSite:"none",
         maxAge:24*60*60*1000
     });
     return token;
@@ -92,7 +92,7 @@ export const adminLogin=async(req,res)=>{
         res.cookie("token",token,{
         httpOnly:true,
         secure:process.env.NODE_ENV === "production",
-        sameSite:"lax",
+        sameSite:"none",
         maxAge:24*60*60*1000
     });
        return res.json({
